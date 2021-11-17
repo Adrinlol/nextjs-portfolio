582a87929e265469de3bb2b743a59e1e1e8feaf2
LashaKakabadze <l.qqbadze@gmail.com>
2021-11-17 23:16:49 +0400

    hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,55 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
+import Image from "next/image";
 import Container from "components/Layout/Container";
+import { StringProps } from "helpers/types";
+import { devices } from "helpers/displayDevice";
+import { Flex } from "components/Layout/Container/styles";
+
+import ProfilePicture from "public/assets/png/Lasha.png";
 
 const Home: NextPage = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <HeroFlex align="flex-start">
+        <ContentWrapper>
+          <RoundImage
+            src={ProfilePicture}
+            alt="Lasha"
+            width="100px"
+            height="100px"
+          />
+          <h2>Welcome to my Portfolio 👋</h2>
+          <p>
+            Passionate React Developer with 3+ years of experience blending the
+            design with programming skills to deliver an immersive and engaging
+            user experience through efficient website development, proactive
+            feature optimization, and relentless debugging.
+          </p>
+        </ContentWrapper>
+      </HeroFlex>
+    </Container>
+  );
 };
 
 export default Home;
+
+export const ContentWrapper = styled.div<StringProps>`
+  h2 {
+    margin: 2rem 0;
+  }
+
+  @media ${devices.mobileL} {
+    margin-top: 2rem;
+  }
+`;
+
+export const RoundImage = styled(Image)`
+  border-radius: 50%;
+`;
+
+const HeroFlex = styled(Flex)`
+  @media ${devices.mobileL} {
+    flex-direction: column-reverse;
+  }
+`;
